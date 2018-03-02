c86d9f0203a8288857316c5850b22fb4efdc085c
Bene_Dev <seriousbenedikt@web.de>
2018-03-02 12:44:37 +0100

    Player dodge (Animation plus physics)

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -14,6 +14,11 @@ public class CharController : MonoBehaviour {
     [SerializeField] float jumpPower;
     [SerializeField] float jumpCap = 3f;
     [SerializeField] float fallMultiplier = 2f;
+    [SerializeField] float dodgePower = 100f;
+    [SerializeField] float dodgeUpPower = 20f;
+
+    [SerializeField] PhysicsMaterial2D normalMaterial;
+    [SerializeField] PhysicsMaterial2D dodgeMaterial;
 
     // Use this for initialization
     void Start () {
@@ -30,7 +35,10 @@ public class CharController : MonoBehaviour {
         CheckForGrounded();
         CheckForJump();
         CheckForInput();
+        CheckForDodge();
     }
+
+    #region Input
 
     private void CheckForInput()
     {
@@ -50,6 +58,42 @@ public class CharController : MonoBehaviour {
         }
     }
 
+    #endregion
+
+    #region Dodge
+
+    private void CheckForDodge()
+    {
+        if(Input.GetButtonDown("Dodge"))
+        {
+            if (transform.localScale == new Vector3(1f, 1f, 1f))
+            {
+                Dodge(1);
+            }
+            else if(transform.localScale == new Vector3(-1f, 1f, 1f))
+            {
+                Dodge(-1);
+            }
+        }
+    }
+
+    private void Dodge(int direction)
+    {
+        anim.SetBool("Dodging", true);
+        rb.sharedMaterial = dodgeMaterial;
+        rb.velocity += new Vector2(dodgePower * speed * direction * Time.deltaTime, dodgeUpPower * Time.deltaTime);
+    }
+
+    private void EndDodge()
+    {
+        anim.SetBool("Dodging", false);
+        rb.sharedMaterial = normalMaterial;
+    }
+
+    #endregion
+
+    #region Jump
+
     private void CheckForJump()
     {
         if (Input.GetButtonDown("Jump") && bGrounded)
@@ -66,9 +110,21 @@ public class CharController : MonoBehaviour {
         }
     }
 
+    private void Jump()
+    {
+        while (rb.velocity.y <= jumpCap)
+        {
+            rb.velocity += new Vector2(0f, input.Jump * jumpPower * Time.deltaTime);
+        }
+    }
+
+    #endregion
+
+    #region Grounded
+
     private void CheckForGrounded()
     {
-        if (Physics2D.Raycast(transform.position + new Vector3(-0.04f, 0f, 0f), Vector2.down, 0.08f) || Physics2D.Raycast(transform.position + new Vector3(0.04f, 0f, 0f), Vector2.down, 0.08f))
+        if (Physics2D.Raycast(transform.position + new Vector3(-0.02f, 0f, 0f), Vector2.down, 0.08f) || Physics2D.Raycast(transform.position + new Vector3(0.02f, 0f, 0f), Vector2.down, 0.08f))
         {
             bGrounded = true;
             anim.SetBool("Grounded", true);
@@ -80,13 +136,7 @@ public class CharController : MonoBehaviour {
         }
     }
 
-    private void Jump()
-    {
-        while (rb.velocity.y <= jumpCap)
-        {
-            rb.velocity += new Vector2(0f, input.Jump * jumpPower * Time.deltaTime);
-        }
-    }
+    #endregion
 
     //private void OnDrawGízmos()
     //{
