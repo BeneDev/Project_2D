afcd101426c5c3f3cea759788b35dfa3e4117b8b
Bene_Dev <seriousbenedikt@web.de>
2018-03-05 21:21:18 +0100

    Bunch of Comments and restructuring

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -9,9 +9,9 @@ public class CharController : MonoBehaviour {
     bool bGrounded = false;
     bool bOnWall = false;
     Animator anim;
-    private Vector3 velocity;
+    private Vector3 velocity; // The value, which is solely allowed to manipulate the transform directly
 
-    public struct PlayerRaycasts
+    public struct PlayerRaycasts // To store the informations of raycasts around the player to calculate physics
     {
         public RaycastHit2D bottomLeft;
         public RaycastHit2D bottomRight;
@@ -34,14 +34,12 @@ public class CharController : MonoBehaviour {
     [SerializeField] float wallSlideSpeed = 3f;
     [SerializeField] float gravity = 2f;
     [SerializeField] float veloYLimit = 10f;
-
-    // Use this for initialization
+    
     void Start () {
         input = GetComponent<PlayerInput>();
         anim = GetComponent<Animator>();
     }
 	
-	// Update is called once per frame
 	void FixedUpdate ()
     {
         #region Raycasts Initialization
@@ -61,14 +59,23 @@ public class CharController : MonoBehaviour {
         // Setting the x velocity
         velocity = new Vector3(input.Horizontal * speed * Time.deltaTime, velocity.y);
 
+        // Checking for collider close to the player
         CheckGrounded();
         CheckOnWall();
 
+        // Checking for actions, the player can do
         CheckForInput();
+        // Start the jumping process if wanted
         CheckForJump();
+        // Start the dodging process if wanted
         CheckForDodge();
-        CheckForAttack();
+        // Checks if the player wants to attack of not
+        if (input.Attack)
+        {
+            Attack();
+        }
 
+        // Checking if the calculated velocity is fine with the world and restrictions
         CheckForValidVelocity();
 
         // Apply gravity
@@ -77,7 +84,7 @@ public class CharController : MonoBehaviour {
             velocity += new Vector3(0, -gravity * Time.deltaTime);
         }
 
-        // Apply the velocity to the transform after checking its validity
+        // Apply the velocity to the transform
         transform.position += velocity;
 
         // Debug feature to test quickly
@@ -87,6 +94,9 @@ public class CharController : MonoBehaviour {
         }
     }
 
+    #region Helper Methods
+
+    // Make sure the velocity does not violate the laws of physics in this game
     private void CheckForValidVelocity()
     {
         // Checking for colliders to the sides
@@ -107,91 +117,10 @@ public class CharController : MonoBehaviour {
             velocity.y = -wallSlideSpeed;
         }
 
-        // Checks if something is above the player and let him bounce down again relative to the force he went up with
+        // Check if something is above the player and let him bounce down again relative to the force he went up with
         if (raycasts.top.collider && velocity.y > 0)
         {
             velocity.y = -velocity.y / 2;
-        }
-    }
-
-    // Resets the players velocity and position to test quickly
-    private void Reset()
-    {
-        transform.position = Vector3.zero;
-        velocity = Vector3.zero;
-    }
-
-    //private void OnGUI()
-    //{
-    //    GUILayout.Label(raycasts.bottomLeft.collider + " right");
-    //    GUILayout.Label(raycasts.bottomRight.collider+ " left");
-    //}
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawRay(transform.position + Vector3.up * 0.03f + Vector3.right * -0.03f, Vector2.right * 0.01f);
-        Gizmos.DrawRay(transform.position + Vector3.up * -0.04f + Vector3.right * -0.03f, Vector2.right * 0.01f);
-    }
-
-    #region Input
-
-    private void CheckForInput()
-    {
-        if(input.Horizontal < 0)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-            anim.SetBool("Idling", false);
-        }
-        else if(input.Horizontal > 0)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-            anim.SetBool("Idling", false);
-        }
-        else
-        {
-            anim.SetBool("Idling", true);
-        }
-    }
-
-    #endregion
-
-    #region Attack
-
-    private void CheckForAttack()
-    {
-        if(input.Attack)
-        {
-            Attack();
-        }
-    }
-
-    private void Attack()
-    {
-        velocity += new Vector3(attackVelocity * transform.localScale.x * Time.deltaTime, 0);
-        anim.SetBool("Attacking", true);
-    }
-
-    private void EndAttack()
-    {
-        anim.SetBool("Attacking", false);
-    }
-
-    #endregion
-
-    #region Dodge
-
-    private void CheckForDodge()
-    {
-        if(input.Dodge && anim.GetBool("Dodging") == false)
-        {
-            anim.SetBool("Dodging", true);
-            appliedDodgeUpPower = dodgeUpPower;
-            Dodge();
-        }
-        if(anim.GetBool("Dodging") == true)
-        {
-            appliedDodgeUpPower -= appliedDodgeUpPower / 10;
-            Dodge();
         }
     }
 
@@ -215,11 +144,110 @@ public class CharController : MonoBehaviour {
         return false;
     }
 
+    // Checks if the player is holding the direction, hes facing in
+    private bool HoldingInDirection()
+    {
+        if (input.Horizontal < 0 && transform.localScale.x < 0)
+        {
+            return true;
+        }
+        else if (input.Horizontal > 0 && transform.localScale.x > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    #endregion
+
+    #region Debugging Tools
+
+    // Resets the players velocity and position to test quickly
+    private void Reset()
+    {
+        transform.position = Vector3.zero;
+        velocity = Vector3.zero;
+    }
+
+    //private void OnGUI()
+    //{
+    //    GUILayout.Label(raycasts.bottomLeft.collider + " right");
+    //    GUILayout.Label(raycasts.bottomRight.collider+ " left");
+    //}
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawRay(transform.position + Vector3.up * 0.03f + Vector3.right * -0.03f, Vector2.right * 0.01f);
+    //    Gizmos.DrawRay(transform.position + Vector3.up * -0.04f + Vector3.right * -0.03f, Vector2.right * 0.01f);
+    //}
+
+    #endregion
+
+    #region Input
+    
+    // Checks if the Player is giving directional input to walk or not
+    private void CheckForInput()
+    {
+        if(input.Horizontal < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            anim.SetBool("Idling", false);
+        }
+        else if(input.Horizontal > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            anim.SetBool("Idling", false);
+        }
+        else
+        {
+            anim.SetBool("Idling", true);
+        }
+    }
+
+    #endregion
+
+    #region Attack
+
+    // Make the player attack
+    private void Attack()
+    {
+        velocity += new Vector3(attackVelocity * transform.localScale.x * Time.deltaTime, 0);
+        anim.SetBool("Attacking", true);
+    }
+
+    // End the attack
+    private void EndAttack()
+    {
+        anim.SetBool("Attacking", false);
+    }
+
+    #endregion
+
+    #region Dodge
+
+    // Set up the dodging process
+    private void CheckForDodge()
+    {
+        if(input.Dodge && anim.GetBool("Dodging") == false)
+        {
+            anim.SetBool("Dodging", true);
+            appliedDodgeUpPower = dodgeUpPower;
+            Dodge();
+        }
+        if(anim.GetBool("Dodging") == true)
+        {
+            appliedDodgeUpPower -= appliedDodgeUpPower / 10;
+            Dodge();
+        }
+    }
+
+    // The actual application of force whilst dodging
     private void Dodge()
     {
         velocity += new Vector3(dodgePower * transform.localScale.x * speed * Time.deltaTime, appliedDodgeUpPower * Time.deltaTime);
     }
 
+    // End the Dodge process
     private void EndDodge()
     {
         anim.SetBool("Dodging", false);
@@ -229,22 +257,26 @@ public class CharController : MonoBehaviour {
 
     #region Jump
 
+    // Start the Jump process
     private void CheckForJump()
     {
         if (input.Jump == 2 && bGrounded || input.Jump == 2 && bOnWall)
         {
             Jump();
         }
+        // Make the player fall less fast when still holding the jump button
         if (input.Jump == 1 && !bGrounded)
         {
             velocity += new Vector3(0f, fallMultiplier * Time.deltaTime);
         }
+        // Make the player fall faster when not holding the jump button anymore
         else if(!bGrounded)
         {
             velocity -= new Vector3(0f, fallMultiplier * Time.deltaTime);
         }
     }
 
+    // The application of the main jumping force
     private void Jump()
     {
         if(bGrounded)
@@ -255,10 +287,12 @@ public class CharController : MonoBehaviour {
 
     #endregion
 
-    #region Grounded
+    #region Grounded and OnWall
 
+    // Checks if the player is on the ground or not
     private void CheckGrounded()
     {
+        // When the bottom left 
         if (raycasts.bottomLeft.collider)
         {
             if (raycasts.bottomLeft.collider.tag == "Ground")
@@ -284,6 +318,7 @@ public class CharController : MonoBehaviour {
         }
     }
 
+    // Checks if the player is up against a wall
     private void CheckOnWall()
     {
         if (HoldingInDirection() && raycasts.lowerLeft.collider || raycasts.upperLeft.collider || raycasts.lowerRight.collider || raycasts.upperRight.collider)
@@ -294,19 +329,6 @@ public class CharController : MonoBehaviour {
         {
             bOnWall = false;
         }
-    }
-
-    private bool HoldingInDirection()
-    {
-        if (input.Horizontal < 0 && transform.localScale.x < 0)
-        {
-            return true;
-        }
-        else if (input.Horizontal > 0 && transform.localScale.x > 0)
-        {
-            return true;
-        }
-        return false;
     }
 
     #endregion
